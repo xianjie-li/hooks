291991026acbbcfb8ca031491ac23e60c83739af
lxj <1073830130@qq.com>
2020-01-10 15:33:28 +0800

    feat: 新增useSessionState系列的钩子

---
+++ src/useSessionSetState.ts
@@ -0,0 +1,20 @@
+import { useCallback } from 'react';
+import { useSessionState } from './useSessionState';
+
+import { SetState } from './useSetState';
+
+/* 与react-use的useSetState一样, 但是额外返回了一个setOverState用于覆盖状态 */
+export const useSessionSetState = <T extends object = any>(
+  key: string,
+  initialState = {} as T,
+): [T, SetState<T>, SetState<T>] => {
+  const [state, set] = useSessionState<T>(key, initialState);
+  const setState = useCallback(
+    patch => {
+      set(prevState => ({ ...prevState, ...(patch instanceof Function ? patch(prevState) : patch) }));
+    },
+    [set],
+  );
+
+  return [state, setState, set as SetState<T>];
+};
---
+++ src/useSessionState.ts
@@ -0,0 +1,56 @@
+import {
+  useState, useCallback,
+  SetStateAction, Dispatch,
+} from 'react';
+
+const BASE_KEY = 'HOOKS_SESSION_STATE_CACHE';
+
+function setSessionState(key: string, beCache: any) {
+  window.sessionStorage.setItem(`${BASE_KEY}_${key.toUpperCase()}`, JSON.stringify(beCache));
+}
+
+function getSessionState(key: string) {
+  const cache = window.sessionStorage.getItem(`${BASE_KEY}_${key.toUpperCase()}`);
+  return cache === null ? cache : JSON.parse(cache);
+}
+
+/**
+ * 与useState一直，区别是会在缓存和加载对值进行sessionStorage存取
+ * */
+function useSessionState<S = undefined>(key: string): [S | undefined, Dispatch<SetStateAction<S | undefined>>];
+function useSessionState<S>(key: string, initialState: S | (() => S)): [S, Dispatch<SetStateAction<S>>];
+function useSessionState<S>(key: string, initialState?: any) {
+  const [state, setState] = useState<S>(() => {
+    const cache = getSessionState(key);
+    if (cache !== null) { // null以外的值都视为缓存
+      return cache;
+    }
+
+    if (initialState instanceof Function) {
+      return initialState();
+    }
+    return initialState;
+  });
+
+  const _setState: typeof setState = useCallback((patch) => {
+    if (patch instanceof Function) {
+      setState(prev => {
+        const patchRes = patch(prev);
+        setSessionState(key, patchRes);
+        return patchRes;
+      });
+    } else {
+      setSessionState(key, patch);
+      setState(patch);
+    }
+    // eslint-disable-next-line
+  }, [setState]);
+
+  return [state, _setState];
+}
+
+export {
+  useSessionState,
+  setSessionState,
+  getSessionState,
+};
