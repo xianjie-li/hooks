1f630171c37bedd108fc3d014a04e4680fe3dfc7
lxj <1073830130@qq.com>
2020-04-13 01:01:36 +0800

    1.x 仓库初始化

--- src/useSelf.ts
+++
@@ -1,7 +0,0 @@
-import { useRef } from 'react';
-
-/** 返回类似类组件的this的实例属性 */
-export function useSelf<T extends object>(init = {} as T) {
-  const self = useRef<T>(init);
-  return self.current as T;
-}
