68f4450794447b1d59db0c319978935ecd05924c
lxj <1073830130@qq.com>
2020-04-17 02:25:11 +0800

    build: 更新文档配置

--- config/config.js
+++ config/config.js
@@ -2,6 +2,8 @@ import path from 'path';
 
 export default {
   outputPath: 'docs',
+  base: '/hooks/',
+  publicPath: '/hooks/',
   title: 'hooks',
   description: 'Use Your Imagination\n',
   resolve: {
