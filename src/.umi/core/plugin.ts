b8fde2bc0c82c7025caa41980d05808e06def736
lxj <1073830130@qq.com>
2020-04-18 15:13:21 +0800

    docs: 添加logo

--- src/.umi/core/plugin.ts
+++
@@ -1,7 +0,0 @@
-import { Plugin } from 'C:/Users/Administrator/Desktop/hooks-next/node_modules/@umijs/runtime/dist/index.js';
-
-const plugin = new Plugin({
-  validKeys: ['patchRoutes','rootContainer','render','onRouteChange',],
-});
-
-export { plugin };
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,38 @@
-
-export * from './state/useSetState';
-
-export * from './state/useSelf';
+/* effect */
+export * from './effect/useCustomEvent';
 
 export * from './effect/useDebounce';
 
-export * from './effect/useThrottle';
+export * from './effect/useEffectEqual';
+
+export * from './effect/useFetch';
 
 export * from './effect/useFn';
+
+export * from './effect/useRRQuery';
+
+export * from './effect/useThrottle';
+
+/* state */
+export * from './state/useDerivedStateFromProps';
+
+export * from './state/useFormState';
+
+export * from './state/useRefize';
+
+export * from './state/useSameState';
+
+export * from './state/useSelf';
+
+export * from './state/useSetState';
+
+export * from './state/useStorageSetState';
+
+export * from './state/useStorageState';
+
+/* UI */
+export * from './ui/useBreakPoint';
+
+export * from './ui/useLockBodyScroll';
+
+export * from './ui/useScroll';
